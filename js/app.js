4ac1be059a174f7c15cd75db001981b6246e4e99
Keith Underwood <kuwood@users.noreply.github.com>
2016-06-08 22:09:13 -0600

    make new game button functional, save user input on submit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,26 @@
+function newGame() {
+  var targetNum = randomInt(1, 101);
+  console.log(targetNum)
+}
+
+function randomInt(min, max) {
+  return ( Math.floor(Math.random() * (max - min + 1)) + min );
+}
 
 $(document).ready(function(){
-	
+  /*--- Start new game on load ---*/
+    newGame();
+  /*--- Start new game on new game click ---*/
+    $(".new").click(function() {
+      newGame();
+    });
+  /*--- Submit guess ---*/
+    $("#guessButton").click(function() {
+      var currentGuess = parseInt($("#userGuess").val());
+      console.log(currentGuess);
+    });
+
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -13,5 +33,3 @@ $(document).ready(function(){
   	});
 
 });
-
-
