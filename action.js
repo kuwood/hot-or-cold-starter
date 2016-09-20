d001b28f62c1482e6e05d2ce390b02918bec3727
kuwood <p4nic510@gmail.com>
2016-09-20 11:34:41 -0600

    Add react, create actions.js

---
+++ action.js
@@ -0,0 +1,23 @@
+// generate winning number
+// making a guess (action)
+// feedback
+// guess history
+// guess number
+// correct guess
+// new game(generate new number)(action)
+// clear guess history
+
+export const ADD_GUESS = 'ADD_GUESS';
+export const addGuess = (number) => {
+    return {
+        type: ADD_GUESS,
+        number: number
+    }
+}
+
+export const NEW_GAME = 'NEW_GAME';
+export const newGame = () => {
+    return {
+        type: NEW_GAME
+    }
+}
